deff2ce4449d660dbc8eb374dab66c41b5c1a77f
Joshua Harpster <joshuaharpter@gmail.com>
2019-09-19 14:42:14 -0400

    Reset all the stuff

---
+++ app/src/main/java/android/grouper/broTeam/CreateNewAccount.java
@@ -0,0 +1,14 @@
+package android.grouper.broTeam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateNewAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_new_account);
+    }
+}
