2705d282a6905ea6be821f6c37b53e07ba184206
Joshua Harpster <joshuaharpter@gmail.com>
2019-09-19 14:42:14 -0400

    Reset all the stuff

--- app/src/main/java/android/grouper/broTeam/LoginMain.java
+++ app/src/main/java/android/grouper/broTeam/LoginMain.java
@@ -1,13 +1,14 @@
 package android.grouper.broTeam;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_main);
     }
 }
