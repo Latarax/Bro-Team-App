bddec934811d02e8f846c5a47d767b7a6177ef73
Joshua Harpster <joshuaharpter@gmail.com>
2019-09-24 12:19:29 -0400

    Not really sure what changed

--- app/src/main/java/android/grouper/broTeam/LoginMain.java
+++ app/src/main/java/android/grouper/broTeam/LoginMain.java
@@ -3,6 +3,8 @@ package android.grouper.broTeam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginMain extends AppCompatActivity {
 
@@ -10,5 +12,27 @@ public class LoginMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
+
+        //*****************************************
+        // this is the code for the log in button *
+        //*****************************************
+        final Button logButton = findViewById(R.id.loginButton);
+        logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                // this is where the code for logging in goes
+            }
+        });
+
+        //*************************************************
+        // This is the code for the create account button *
+        //*************************************************
+        final Button createAccButton = findViewById(R.id.createAccountButton);
+        createAccButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view2) {
+                // this is the code for going to create account page
+            }
+        });
     }
 }
