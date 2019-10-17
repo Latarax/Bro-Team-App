e156c1b3eda0f515153d0ac03d04830a704073f9
Latarax <talhakhan004@gmail.com>
2019-10-17 15:13:50 -0400

    added ability to create accounts and login

--- app/src/main/java/android/grouper/broTeam/LoginMain.java
+++ app/src/main/java/android/grouper/broTeam/LoginMain.java
@@ -1,20 +1,39 @@
 package android.grouper.broTeam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 import static java.security.AccessController.getContext;
 
 public class LoginMain extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
+    EditText usernameField, passwordField;
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        usernameField = findViewById(R.id.usernameInput);
+        passwordField = findViewById(R.id.passwordInput);
+        progressBar = findViewById(R.id.progressBar);
 
         //*****************************************
         // this is the code for the log in button *
@@ -23,11 +42,7 @@ public class LoginMain extends AppCompatActivity {
         logButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view1) {
-                // this is where the code for logging in goes
-
-                // presumably bundle and send data to fire base
-
-                // presumably wait for firebase to confirm
+                userLogin();
 
                 // If valid account, create intent to go to user main page
 
@@ -48,4 +63,44 @@ public class LoginMain extends AppCompatActivity {
             }
         });
     }
+
+    private void userLogin() {
+        String email = usernameField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            usernameField.setError("Must enter an email address");
+            usernameField.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            passwordField.setError("Must enter a password");
+            passwordField.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if(task.isSuccessful()){
+                    Intent goToHome = new Intent(LoginMain.this, HomeGroupList.class);
+                    goToHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(goToHome);
+                }
+
+                else {
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException) {
+                        Toast.makeText(getApplicationContext(), "This email is already in use", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            }
+        );}
 }
