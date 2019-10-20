74fae00d07703f69e1b81f9d0e6a41ac42994687
Isaiah Martinez <martinez.isaiah32@hotmail.com>
2019-10-20 19:17:46 -0400

    readded card click functionality

--- app/src/main/java/android/grouper/broTeam/GroupCardHolder.java
+++ app/src/main/java/android/grouper/broTeam/GroupCardHolder.java
@@ -12,11 +12,18 @@ public class GroupCardHolder extends RecyclerView.ViewHolder {
     ImageView mImgView;
     TextView mTitle, mDescription;
 
+    cardClickListener mCardClickListener;
+
     public GroupCardHolder(@NonNull View itemView) {
         super(itemView);
 
         this.mDescription = itemView.findViewById(R.id.descriptionIv);
         this.mTitle = itemView.findViewById(R.id.titleIv);
         this.mImgView = itemView.findViewById(R.id.imageIv);
+    }
+
+
+    public void setCardClickListener(cardClickListener mCardClickListener) {
+        this.mCardClickListener = mCardClickListener;
     }
 }
