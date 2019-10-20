464ca6d60a2238222748742baa62aff923399efc
Joshua Harpster <joshuaharpter@gmail.com>
2019-10-20 17:23:07 -0400

    Did some stuff which is useful

---
+++ app/src/main/java/android/grouper/broTeam/GroupTaskDisplay.java
@@ -0,0 +1,14 @@
+package android.grouper.broTeam;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GroupTaskDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_task_display);
+    }
+}
