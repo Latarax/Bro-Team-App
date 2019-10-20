130d988931677610cc70e22b0365967faa9d37cb
Isaiah Martinez <martinez.isaiah32@hotmail.com>
2019-10-20 19:07:52 -0400

    readding card click

--- app/src/main/java/android/grouper/broTeam/GroupTaskDisplay.java
+++ app/src/main/java/android/grouper/broTeam/GroupTaskDisplay.java
@@ -1,14 +1,32 @@
 package android.grouper.broTeam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GroupTaskDisplay extends AppCompatActivity {
 
+    TextView mTitleTv, mDescTv;
+    ImageView mImageV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_task_display);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        mTitleTv = findViewById(R.id.titleIv);
+
+        Intent intent = getIntent();
+
+        String mTitle = intent.getStringExtra("iTitle");
+
+        actionBar.setTitle(mTitle);
+
+        mTitleTv.setText(mTitle);
     }
 }
---
+++ app/src/main/java/android/grouper/broTeam/cardClickListener.java
@@ -0,0 +1,7 @@
+package android.grouper.broTeam;
+
+import android.view.View;
+
+public interface cardClickListener {
+    void onCardClickListener(View v, int position);
+}
