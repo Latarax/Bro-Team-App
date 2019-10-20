38e3c33fee972707efb32fd839ebcebde57a92e0
Isaiah Martinez <martinez.isaiah32@hotmail.com>
2019-10-20 19:24:31 -0400

    fixed group card holder

--- app/src/main/java/android/grouper/broTeam/GroupCardHolder.java
+++ app/src/main/java/android/grouper/broTeam/GroupCardHolder.java
@@ -7,11 +7,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class GroupCardHolder extends RecyclerView.ViewHolder {
+public class GroupCardHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     ImageView mImgView;
     TextView mTitle, mDescription;
-
     cardClickListener mCardClickListener;
 
     public GroupCardHolder(@NonNull View itemView) {
@@ -20,8 +19,14 @@ public class GroupCardHolder extends RecyclerView.ViewHolder {
         this.mDescription = itemView.findViewById(R.id.descriptionIv);
         this.mTitle = itemView.findViewById(R.id.titleIv);
         this.mImgView = itemView.findViewById(R.id.imageIv);
+
+        itemView.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        this.mCardClickListener.onCardClickListener(view, getLayoutPosition());
+    }
 
     public void setCardClickListener(cardClickListener mCardClickListener) {
         this.mCardClickListener = mCardClickListener;
