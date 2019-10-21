2ccb5375af42c28373e7f8d7287f9337164e96bc
martinezi <martinez.isaiah32@hotmail.com>
2019-10-21 19:38:27 -0400

    readded cards

--- app/src/main/java/android/grouper/broTeam/HomeGroupList.java
+++ app/src/main/java/android/grouper/broTeam/HomeGroupList.java
@@ -3,18 +3,22 @@ package android.grouper.broTeam;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /*
  #####################################################
@@ -44,37 +48,57 @@ public class HomeGroupList extends AppCompatActivity {
 
     private ArrayList<CardModel> getMyList(String email) {
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("userList");
 
-        Query query = myRef.child(email).getParent();
+        final ArrayList<CardModel> models = new ArrayList<>();
+        final ArrayList<String> groups = new ArrayList<>();
 
-        myRef.child("usersList").getKey();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        CollectionReference myRef = database.collection("userList/" + user.getUid());
+
+        myRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                for(DocumentSnapshot document : task.getResult()) {
+                    List<String> list = (List<String>) document.get("groupList");
+                    for (String s : list){
+                        groups.add(s);
+                    }
+                }
             }
         });
 
+        if(groups.size() == 0)
+        {
+            return models;
+        }
 
-        ArrayList<CardModel> models = new ArrayList<>();
+        for(final String gid: groups) {
+            CollectionReference ref = database.collection("groupsList");
+            ref.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    for (DocumentSnapshot document : task.getResult()) {
+                        if (document.getId().contentEquals(gid)) {
+                            CardModel m = new CardModel();
+                            m.setTitle(document.get("groupName").toString());
+                            m.setDescription(document.get("description").toString());
+                            m.setImg(R.drawable.ic_launcher_foreground);
+                            models.add(m);
+                        }
+                    }
+                }
+            });
+        }
 
-        CardModel m = new CardModel();
-        m.setTitle("NewsFeed");
-        m.setDescription("This is a newsfeed description");
-        m.setImg(R.drawable.ic_launcher_background);
-        models.add(m);
-
+        for(int i = 0; i < 4; i++) {
+            CardModel m = new CardModel();
+            m.setTitle("NewsFeed");
+            m.setDescription("This is a newsfeed description");
+            m.setImg(R.drawable.ic_launcher_background);
+            models.add(m);
+        }
         return models;
     }
 }
