8645252dc05f5e0eb9d147afc0204f6e406382a4
martinezi <martinez.isaiah32@hotmail.com>
2019-10-22 17:15:56 -0400

    map to get data

---
+++ app/src/main/java/android/grouper/broTeam/CreateNewGroup.java
@@ -0,0 +1,14 @@
+package android.grouper.broTeam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateNewGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_group);
+    }
+}
