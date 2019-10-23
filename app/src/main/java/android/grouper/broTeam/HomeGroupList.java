fd7a385d432790b653c311df65bbd7decefc26fd
martinezi <martinez.isaiah32@hotmail.com>
2019-10-23 17:59:08 -0400

    attempt to get group data

--- app/src/main/java/android/grouper/broTeam/HomeGroupList.java
+++ app/src/main/java/android/grouper/broTeam/HomeGroupList.java
@@ -17,7 +17,6 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
-import java.util.Map;
 
 /*
  #####################################################
@@ -46,85 +45,77 @@ public class HomeGroupList extends AppCompatActivity {
 
 
         ArrayList<CardModel> models = new ArrayList<>();
-        final ArrayList<Object> groups = new ArrayList<>();
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         FirebaseFirestore database = FirebaseFirestore.getInstance();
+
         DocumentReference userId = database.collection("usersList").document(user.getUid());
-         userId.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-             @Override
-             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                 if (task.isSuccessful()) {
-                     DocumentSnapshot document = task.getResult();
-                     if (document.exists()) {
-                         Log.d("DocumentSnapshot data: ", "" + document.getData());
-                         if(!document.getData().containsKey("groupList")){
-                             Log.d("no array", "No groupList");
-                         } else {
-                             Map<String, Object> data = document.getData();
-                             ArrayList<DocumentReference> c = (ArrayList<DocumentReference>) document.get("groupList");
-                             Log.d("groups", ""+c);
-                             Log.d("map", ""+data);
-                             Log.d("array", ""+data.get("groupList"));
-                         }
-                     } else {
-                         Log.d("no field", "No such document");
-                     }
-                 } else {
-                     Log.d("failed to get", "get failed with ", task.getException());
-                 }
-             }
-         });
+        userId.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                Log.d("task group call", ""+task.getResult());
+                if (task.isSuccessful()) {
+                    final DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("DocumentSnapshot data: ", "" + document.getData());
+                        if(!document.getData().containsKey("groupList")){
+                            Log.d("no array", "No groupList");
 
-         if(groups.isEmpty()) {
-             CardModel m = new CardModel();
-             m.setTitle("Bro Team");
-             m.setDescription("Just a bunch of bro's");
-             m.setImg(R.drawable.ic_launcher_background);
-             models.add(m);
+                        } else {
 
-             CardModel p = new CardModel();
-             p.setTitle("Party team");
-             p.setDescription("Lets get liiiiit");
-             p.setImg(R.drawable.ic_launcher_background);
-             models.add(p);
+                            //Map<String, Object> data = document.getData();
+                            ArrayList<DocumentReference> groups = (ArrayList<DocumentReference>) document.get("groupList");
 
-             CardModel f = new CardModel();
-             f.setTitle("Friends");
-             f.setDescription("Lets help each other out");
-             f.setImg(R.drawable.ic_launcher_background);
-             models.add(f);
+                            for(int i = 0; i < groups.size(); i++){
 
-             CardModel r = new CardModel();
-             r.setTitle("Roomates");
-             r.setDescription("For keeping the apartment clean");
-             r.setImg(R.drawable.ic_launcher_background);
-             models.add(r);
-             return models;
-         }
+                                Log.d("group index", ""+groups.get(i).getId());
+                                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                DocumentReference groupId = db.collection("groupsList").document(groups.get(i).getId());
 
-        Log.d("groups", ""+groups);
-        for(Object gid: groups) {
-            Log.d("groups array", ""+gid);
-            /*CollectionReference groupId = database.collection();
-            database.collection(gid)
-                    .whereEqualTo(gid.toString(), true)
-                    .get()
-                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                            if(task.isSuccessful()) {
-                                for (QueryDocumentSnapshot document : task.getResult()) {
-                                    CardModel m = new CardModel();
-                                    m.setTitle(document.get("groupName").toString());
-                                    m.setDescription(document.get("description").toString());
-                                    m.setImg(R.drawable.ic_launcher_foreground);
-                                    models.add(m);
-                                }
+                                groupId.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> t) {
+                                        if(t.isSuccessful()){
+                                            Log.d("task group call", ""+t.getResult());
+                                            DocumentSnapshot documentSnapshot = t.getResult();
+                                            Log.d("DocumentSnapshot group", ""+documentSnapshot.getData());
+                                        }
+                                    }
+                                });
                             }
                         }
-                    });*/
-        }
+                    } else {
+                        Log.d("no field", "No such document");
+                    }
+                } else {
+                    Log.d("failed to get", "get failed with ", task.getException());
+                }
+            }
+        });
+
+        CardModel m = new CardModel();
+        m.setTitle("Bro Team");
+        m.setDescription("Just a bunch of bro's");
+        m.setImg(R.drawable.ic_launcher_background);
+        models.add(m);
+
+        CardModel p = new CardModel();
+        p.setTitle("Party team");
+        p.setDescription("Lets get liiiiit");
+        p.setImg(R.drawable.ic_launcher_background);
+        models.add(p);
+
+        CardModel f = new CardModel();
+        f.setTitle("Friends");
+        f.setDescription("Lets help each other out");
+        f.setImg(R.drawable.ic_launcher_background);
+        models.add(f);
+
+        CardModel r = new CardModel();
+        r.setTitle("Roommates");
+        r.setDescription("For keeping the apartment clean");
+        r.setImg(R.drawable.ic_launcher_background);
+        models.add(r);
 
         return models;
     }
---
+++ app/src/main/java/android/grouper/broTeam/MyCallBack.java
@@ -0,0 +1,7 @@
+package android.grouper.broTeam;
+
+import java.util.ArrayList;
+
+public interface MyCallBack {
+    void onCallBack(ArrayList<CardModel> models);
+}
