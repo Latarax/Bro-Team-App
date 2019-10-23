2dbe5df7cb5b57a9dbc033c1b2ff6a425b9b6954
NuclearIcecream <NuclearIcecream@users.noreply.github.com>
2019-10-23 14:55:04 -0400

    Menu in

--- app/src/main/java/android/grouper/broTeam/GroupChatDisplay.java
+++ app/src/main/java/android/grouper/broTeam/GroupChatDisplay.java
@@ -1,14 +1,50 @@
 package android.grouper.broTeam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class GroupChatDisplay extends AppCompatActivity {
+
+    BottomNavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_chat_display);
+
+
+        // THIS IS FOR THE BOTTOM NAV VIEW DO NOT TOUCH UNLESS KNOW WHAT DOING
+        navigation = findViewById(R.id.bottomNavView);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.groupTasksItem:
+                        Intent a = new Intent(GroupChatDisplay.this, GroupTaskDisplay.class);
+                        startActivity(a);
+                        break;
+                    case R.id.groupUsersItem:
+                        Intent b = new Intent(GroupChatDisplay.this, GroupUsersDisplay.class);
+                        startActivity(b);
+                        break;
+                    case R.id.groupNavItem:
+                        Intent c = new Intent(GroupChatDisplay.this, GroupWaypointDisplay.class);
+                        startActivity(c);
+                        break;
+                    case R.id.groupChatItem:
+                        Intent d = new Intent(GroupChatDisplay.this, GroupChatDisplay.class);
+                        startActivity(d);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/android/grouper/broTeam/GroupTaskDisplay.java
+++ app/src/main/java/android/grouper/broTeam/GroupTaskDisplay.java
@@ -2,16 +2,22 @@ package android.grouper.broTeam;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class GroupTaskDisplay extends AppCompatActivity {
 
     TextView mTitleTv, mDescTv;
     ImageView mImageV;
+    BottomNavigationView navigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +34,33 @@ public class GroupTaskDisplay extends AppCompatActivity {
         actionBar.setTitle(mTitle);
 
         mTitleTv.setText(mTitle);
+
+
+        // THIS IS FOR THE BOTTOM NAV VIEW DO NOT TOUCH UNLESS KNOW WHAT DOING
+        navigation = findViewById(R.id.bottomNavView);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.groupTasksItem:
+                        // CURRENT ACTIVITY
+                        break;
+                    case R.id.groupUsersItem:
+                        Intent b = new Intent(GroupTaskDisplay.this, GroupUsersDisplay.class);
+                        startActivity(b);
+                        break;
+                    case R.id.groupNavItem:
+                        Intent c = new Intent(GroupTaskDisplay.this, GroupWaypointDisplay.class);
+                        startActivity(c);
+                        break;
+                    case R.id.groupChatItem:
+                        Intent d = new Intent(GroupTaskDisplay.this, GroupChatDisplay.class);
+                        startActivity(d);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
