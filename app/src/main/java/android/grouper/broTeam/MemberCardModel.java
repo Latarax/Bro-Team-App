b119b8aafbd05896810089ab34343c31108317bb
Latarax <talhakhan004@gmail.com>
2019-11-07 13:42:51 -0500

    members within group are visible on members page

---
+++ app/src/main/java/android/grouper/broTeam/MemberCardModel.java
@@ -0,0 +1,13 @@
+package android.grouper.broTeam;
+
+public class MemberCardModel {
+    private String title;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
